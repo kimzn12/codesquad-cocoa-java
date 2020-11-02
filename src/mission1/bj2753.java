b2ad0a67d30149e52d29a032d9144269f1b3611a
kim <kim@gmail.com>
2020-11-02 17:30:59 +0900

    .

---
+++ src/mission1/bj2753.java
@@ -0,0 +1,24 @@
+/*
+윤년이면 1, 아니면 0
+
+ */
+package mission1;
+
+import java.util.Scanner;
+
+public class bj2753 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int year = sc.nextInt();
+
+        if((year % 4 == 0 && year%100 !=0) || (year/400 == 0)){
+            System.out.println("1");
+        }
+        else{
+            System.out.println("0");
+        }
+
+    }
+
+}
