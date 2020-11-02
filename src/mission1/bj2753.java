83460548fb3868af8442407a3eb4b686f4892fda
kim <kim@gmail.com>
2020-11-02 17:42:34 +0900

    .

--- src/mission1/bj2753.java
+++ src/mission1/bj2753.java
@@ -12,7 +12,7 @@ public class bj2753 {
 
         int year = sc.nextInt();
 
-        if((year % 4 == 0 && year%100 !=0) || (year/400 == 0)){
+        if((year % 4 == 0 && year % 100 !=0) || (year%400 == 0)){
             System.out.println("1");
         }
         else{
