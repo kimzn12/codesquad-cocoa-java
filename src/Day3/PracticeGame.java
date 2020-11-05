5959436d5deea5b9bde5d9305454e95e071e43b3
kimzn12 <kimzn12@naver.com>
2020-11-05 23:58:42 +0900

    Complete hero game

--- src/Day3/PracticeGame.java
+++ src/Day3/PracticeGame.java
@@ -1,6 +1,5 @@
 package Day3;
 
-import java.util.Scanner;
 
 class Hero {
     public String name;
@@ -11,26 +10,30 @@ class Hero {
         this.hp = 100;
     }
 
+    //h를 공격
+    public void punch(Hero h){
+        h.hp -= 30;
+        System.out.println(this.name + "의 공격!");
+        System.out.println(h.name + "의 체력: " + h.hp);
+        System.out.println();
+    }
+
     public static void battle(Hero h1, Hero h2) {
         //왜 static..?
+        Hero attacker, depender;
 
         while(h1.hp > 0 && h2.hp > 0) {
-            Scanner sc = new Scanner(System.in);
-            System.out.println(h1.name + "또는" + h2.name + "을 입력하세요.");
-            String attacker = sc.next();
-            //sc.close();
-
-            if (attacker.equals(h1.name)) {
-                h2.hp -= 30;
-                System.out.println(h1.name + "의 공격!");
-                System.out.println(h2.name + "의 체력: " + h2.hp);
+            if(Math.random() < 0.5) { // 0.00... ~ 0.9999 절반확률 : <0.5
+                attacker = h1;
+                depender = h2;
             }
             else {
-                h1.hp -= 30;
-                System.out.println(h2.name + "의 공격!");
-                System.out.println(h1.name + "의 체력: " + h1.hp);
+                attacker = h2;
+                depender = h1;
             }
+            attacker.punch(depender);
         }
+
         if (h1.hp ==0){
             System.out.println(h2.name + "이 이겼습니다.");
         }
