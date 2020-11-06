9e9ced3998d87c1931b7bf91fde0e06021b105bf
kimzn12 <kimzn12@naver.com>
2020-11-06 12:32:57 +0900

    아스키로변환

---
+++ src/AL/BJ11654.java
@@ -0,0 +1,14 @@
+package AL;
+
+import java.util.Scanner;
+
+public class BJ11654 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        char c = sc.next().charAt(0);
+
+        System.out.println((int)c);
+
+    }
+}
