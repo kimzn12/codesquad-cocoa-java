5d02ca83e7e3b730b3d4c0c3ae45333729e9fa93
kimzn12 <kimzn12@naver.com>
2020-11-26 04:15:52 +0900

    버튼 추가

--- src/mission8/Paint/PaintFrame.java
+++ src/mission8/Paint/PaintFrame.java
@@ -7,73 +7,100 @@ import java.awt.event.*;
 public class PaintFrame extends JFrame {
     public int startX, startY, endX, endY;
 
-    JRadioButton rLine;
-    JRadioButton rOval;
-    JRadioButton rRect;
-    JRadioButton rFreeLine;
+    Button bFreeLine = new Button("Pencil");
+    Button bLine = new Button("Line");
+    Button bOval = new Button("circle");
+    Button bRect = new Button("Rectangle");
+    Button bClear = new Button("clear");
+
+    JPanel panel;
 
     boolean drawLine = false;
     boolean drawOval = false;
     boolean drawRect = false;
     boolean drawFreeLine = false;
+    boolean clear = true;
 
     public PaintFrame(String title) {
         //윈도우 설정
         setTitle(title);
         setDefaultCloseOperation(EXIT_ON_CLOSE); //닫힘
         setSize(1200, 1200);
-        setLayout(new FlowLayout());
+        setLayout(new BorderLayout());
 
-        rFreeLine = new JRadioButton("연필");
-        rLine = new JRadioButton("직선");
-        rOval = new JRadioButton("원");
-        rRect = new JRadioButton("사각형");
+        //패널 생성
+        panel = new JPanel(new FlowLayout());
+        panel.setBackground(Color.BLACK);
 
         //라디오버튼 생성
         makeRadioButton();
 
         //이벤트 리스너 추가
-        addMouseListener(new MyMouseListener());
         addMouseMotionListener(new MyMouseMotionListener());
+        addMouseListener(new MyMouseListener());
+
 
         setVisible(true);
     }
 
     //라디오 버튼 생성,리스너추가
     public void makeRadioButton() {
-        rFreeLine = new JRadioButton("연필");
-        rLine = new JRadioButton("직선");
-        rOval = new JRadioButton("원");
-        rRect = new JRadioButton("사각형");
-
-        //버튼 중 하나만 선택
-        ButtonGroup bg = new ButtonGroup();
-        bg.add(rFreeLine);
-        bg.add(rLine);
-        bg.add(rOval);
-        bg.add(rRect);
-
-
-        //프레임에 버튼 추가
-        add(rFreeLine);
-        add(rLine);
-        add(rOval);
-        add(rRect);
-
         //리스너 등록
-        rOval.addActionListener(e -> drawOval = true);
-        rLine.addActionListener(e -> drawLine = true);
-        rRect.addActionListener(e -> drawRect = true);
-        rFreeLine.addActionListener(e -> drawFreeLine = true);
+        bOval.addActionListener(e -> {
+            drawOval = true;
+            drawLine = false;
+            drawFreeLine = false;
+            drawRect = false;
+            clear = false;
+
+        });
+
+        bRect.addActionListener(e -> {
+            drawOval = false;
+            drawLine = false;
+            drawFreeLine = false;
+            drawRect = true;
+            clear = false;
+
+        });
+
+        bLine.addActionListener(e -> {
+            drawOval = false;
+            drawLine = true;
+            drawFreeLine = false;
+            drawRect = false;
+            clear = false;
+        });
+
+        bFreeLine.addActionListener(e -> {
+            drawOval = false;
+            drawLine = false;
+            drawFreeLine = true;
+            drawRect = false;
+            clear = false;
+        });
+
+        bClear.addActionListener(e -> {
+            drawOval = false;
+            drawLine = false;
+            drawFreeLine = false;
+            drawRect = false;
+            clear = true;
+        });
+
+        panel.add(bFreeLine);
+        panel.add(bLine);
+        panel.add(bOval);
+        panel.add(bRect);
+        panel.add(bClear);
+
+        //add(radioPanel,BorderLayout.NORTH);
+        add("North", panel);
+
     }
 
     //내부 클래스로 리스너 구현
-    class MyMouseListener extends MouseAdapter {
-        @Override
-        public void mouseClicked(MouseEvent e) {
-
-        }
-
+    class MyMouseListener extends MouseAdapter { // 도형그릴 때
         @Override
         public void mousePressed(MouseEvent e) {
             System.out.println("pressed");
@@ -91,27 +118,32 @@ public class PaintFrame extends JFrame {
 
     }
 
-    class MyMouseMotionListener implements MouseMotionListener {
+    class MyMouseMotionListener extends MouseMotionAdapter {  // 자유선그릴때
 
         @Override
         public void mouseDragged(MouseEvent e) {
-
-            startX = e.getX();
-            startY = e.getY();
-            repaint();
-
-        }
-
-        @Override
-        public void mouseMoved(MouseEvent e) {
+            if(drawFreeLine){
+                startX = e.getX();
+                startY = e.getY();
+                repaint();
+            }
         }
     }
 
     public void paint(Graphics g) {
+
         g.setColor(Color.BLACK);
 
+        if(clear){
+            g.clearRect(0, 0, 1200, 1200); //화면지우는코드
+        }
+
         if (drawFreeLine) {
+
             g.drawString("●", startX, startY);
+
+//            g.drawString("●", startX, startY);
+//            g.drawString("●", startX+1, startY+1);
         }
 
         //직선
